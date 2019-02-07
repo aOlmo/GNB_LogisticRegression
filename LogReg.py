97167738596886d68fa109185f3d4097f3b87d95
Alberto Olmo <aolmoher@uccs.edu>
2019-02-07 11:35:36 -0700

    Accuracy() and predict() created for LogReg

--- LogReg.py
+++ LogReg.py
@@ -10,7 +10,6 @@ for i, name in enumerate(dataset_layout):
     if name == "class":
         c_pos = i
 
-
 # ================== Gathering the data ================== #
 data_fname = 'data_banknote_authentication.txt'
 df = pd.read_csv(data_fname, sep=',', header=None)
@@ -22,34 +21,15 @@ n_total_rows = X.shape[0]
 # Get all data values for Class 0 and Class 1
 c0_X = df.loc[df[c_pos] == cs[0]].iloc[:, 0:c_pos].values
 c1_X = df.loc[df[c_pos] == cs[1]].iloc[:, 0:c_pos].values
-n_c0 = c0_X.shape[0]
-n_c1 = c1_X.shape[0]
 # ================== ================== ================== #
 
 # - Define the Logistic function
 def sigmoid(z):
     return 1 / (1 + np.exp(-z))
 
-def gradient_ascent(X, h, y):
-    return np.dot(X.T, y - h)
-
-def loss(h, y):
-    return (-y * np.log(h) - (1 - y) * np.log(1 - h)).mean()
-
 def log_likelihood(X, y, theta):
     scores = np.dot(X, theta)
-    ll = np.sum(y*scores - np.log(1 + np.exp(scores)))
-    return ll
-
-# lr = 0.001
-# theta = np.zeros(X.shape[1])
-# for i in range(10000):
-#     z = np.dot(X, theta)
-#     h = sigmoid(z)
-#     theta -= lr * gradient_ascent(X, h, y)
-#     if i % 1000:
-#         print(loss(h, y))
-
+    return np.sum(y*scores - np.log(1 + np.exp(scores)))
 
 def logistic_regression(X, y, num_steps, learning_rate):
     weights = np.zeros(X.shape[1])
@@ -65,12 +45,30 @@ def logistic_regression(X, y, num_steps, learning_rate):
 
         # Print log-likelihood every so often
         if step % 1000 == 0:
-            print(log_likelihood(X, y, weights))
+            print("Loss: {}".format(log_likelihood(X, y, weights)))
 
     return weights
 
+def predict(X, weights):
+    return np.round(sigmoid(np.dot(X, weights)))
 
-weights = logistic_regression(X, y, 10000, 0.001)
+def accuracy(X_test, y_test, weights):
+    n = X_test.shape[0]
+    corrects = []
+    for x, y in zip(X_test, y_test):
+        y_pred = predict(x, weights)
+        corrects.append(y_pred - y)
 
-t = sigmoid(np.dot(c0_X[99], weights))
-print(t)
+    corrects = np.array(corrects)
+    total_preds = np.count_nonzero(corrects == 0)
+    return total_preds/n
+
+
+if __name__ == '__main__':
+    X_test = c1_X
+    y_test = [1]*c0_X.shape[0]
+
+    weights = logistic_regression(X, y, 10000, 0.001)
+
+    acc = accuracy(X_test, y_test, weights)
+    print("The accuracy is: {}".format(acc))
